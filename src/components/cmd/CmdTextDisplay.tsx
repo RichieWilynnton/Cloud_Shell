2ee0b523fe310c42d67c45556c793636542a812c
RichieWilynnton <richiewillynton@gmail.com>
2024-04-03 03:15:10 +0800

    fixed date to showing time

--- src/components/cmd/CmdTextDisplay.tsx
+++ src/components/cmd/CmdTextDisplay.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from 'react';
 
-// Display the currect directory + command + date
-const CmdTextDisplay = ({cmd, date} : {cmd: string, date: Date}) => {
+// Display the current directory + command + date or time
+const CmdTextDisplay = ({ cmd, date }: { cmd: string, date: Date }) => {
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
+  const dateForComparison = new Date(date.getTime());
+  dateForComparison.setHours(0, 0, 0, 0);
+
+  const isToday = dateForComparison.getTime() === today.getTime();
+
+  const displayDate = isToday ? date.toLocaleTimeString() : date.toLocaleDateString();
+
   return (
     <div className='flex justify-between items-center '>
-        <div className='text-gray-300 text-xl'> {`<C:\\users\\richie> ${cmd}`}</div>
-        <div className='text-gray-300 text-xl '>{date.toLocaleDateString()}</div>
+      <div className='text-gray-300 text-xl'>{`<C:\\users\\richie> ${cmd}`}</div>
+      <div className='text-gray-300 text-xl'>{displayDate}</div>
     </div>
-  )
+  );
 }
 
-export default CmdTextDisplay
+export default CmdTextDisplay;
--- src/components/cmd/EnteredCmd.tsx
+++ src/components/cmd/EnteredCmd.tsx
@@ -1,12 +1,10 @@
 import { CmdI } from '@/interfaces/CmdI'
 import React from 'react'
 import CmdTextDisplay from './CmdTextDisplay'
-import CmdError from './cmdOutputs/CmdError'
 import { CmdProps } from '@/interfaces/CmdPropsI'
 
 // Displays history of entered commands
 const EnteredCmd = ({ cmdHistory } : { cmdHistory: CmdI[] }) => {
-    console.log(cmdHistory);
     return (
         <div>
                 {cmdHistory.map(({ cmd, Component, props, time }, index) => (
