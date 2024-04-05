058485acd7f6e83db0a4cf838afe3ab730ec916b
RichieWilynnton <richiewillynton@gmail.com>
2024-04-06 02:39:28 +0800

    fixed navbar to top of terminal

--- src/components/cmd/Navbar.tsx
+++ src/components/cmd/Navbar.tsx
@@ -2,10 +2,9 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <div>
-            <div>
-                <div className="flex justify-between items-center bg-gray-800 p-2">
-                    <div className="flex">
+            <div className=""> 
+                <div className="text-gray-300 max-w-4xl mx-auto border-x-2 border-t-2 border-slate-800 rounded-t-md bg-slate-800 p-2 flex items-center justify-between">
+                    <div className="flex gap-2">
                         <div className="h-3 w-3 rounded-full bg-gray-700"></div>
                         <div className="ml-1 h-3 w-3 rounded-full bg-gray-700"></div>
                         <div className="ml-1 h-3 w-3 rounded-full bg-gray-700"></div>
@@ -18,7 +17,6 @@ const Navbar = () => {
                     </div>
                 </div>
             </div>
-        </div>
     );
 };
 
--- src/components/cmd/TerminalBox.tsx
+++ src/components/cmd/TerminalBox.tsx
@@ -1,36 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import { CmdI } from '../../interfaces/CmdI'
-import Header from './Header';
-import EnteredCmd from './EnteredCmd';
-import CmdInput from './CmdInput';
-import Navbar from './Navbar';
-import processCMD from '@/utils/processCmd';
-import { useAppContext } from './context/AppContext';
+import React, { useEffect, useRef, useState } from "react";
+import { CmdI } from "../../interfaces/CmdI";
+import Header from "./Header";
+import EnteredCmd from "./EnteredCmd";
+import CmdInput from "./CmdInput";
+import Navbar from "./Navbar";
+import processCMD from "@/utils/processCmd";
+import { useAppContext } from "./context/AppContext";
 
-// Main terminal box 
+// Main terminal box
 const TerminalBox = () => {
     const [cmdState, setCmdState] = useState<CmdI[]>([]);
     const appContext = useAppContext(); // React Rule of Hooks - hooks must be called at the top level of components
+    const dummyRef = useRef() as React.MutableRefObject<HTMLDivElement> // For scrolling to bottom
+
+    useEffect(() => {
+		dummyRef.current.scrollIntoView({ behavior: "auto" });
+	}, [cmdState]);
 
     const handleSubmit = (cmd: string) => {
         setCmdState((currCmd) => [
             ...currCmd,
-            {...processCMD(cmd, appContext), time: new Date()}
+            { ...processCMD(cmd, appContext), time: new Date() },
         ]);
-    }
+    };
 
     return (
         <div>
-            <div className="max-w-4xl border-x-2 border-b-2 border-slate-800 rounded-b-md mx-auto text-gray-300 text-xl overflow-y-auto h-55vh bg-black bg-opacity-75 box">
-                <Navbar />
-                <div className='p-2'>
-                <Header />
-                <EnteredCmd cmdHistory={cmdState} />
-                <CmdInput onSubmit={handleSubmit}/>
+            <Navbar />
+            <div className="box mx-auto h-55vh max-w-4xl overflow-y-auto rounded-b-md border-x-2 border-b-2 border-slate-800 bg-black bg-opacity-75 text-xl text-gray-300">
+                <div className="p-2">
+                    <Header />
+                    <EnteredCmd cmdHistory={cmdState} />
+                    <CmdInput onSubmit={handleSubmit} />
+                    <div ref={dummyRef}></div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default TerminalBox
+export default TerminalBox;
